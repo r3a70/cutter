02b077d955805c3c4212a678cffcb50cd90f87ef
r3a96 <r3a96.info@gmail.com>
2024-03-14 11:02:17 +0330

    setup for publish to pypi

---
+++ setup.py
@@ -0,0 +1,13 @@
+from setuptools import setup
+
+
+setup(
+    name="RCutter",
+    version="0.0.4",
+    install_requires=[
+        'ffmpeg-python==0.2.0'
+    ],
+    author="surena",
+    author_email="ram3a1999.info@gmail.com",
+    url="https://github.com/r3a70/cutter"
+)
